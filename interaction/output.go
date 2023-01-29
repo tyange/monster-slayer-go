4ad1814472e34cb9d019da2479b648c32f4d6fc0
tyange <usun16@gmail.com>
2023-01-29 17:31:45 +0900

    code for 'go build' command

--- interaction/output.go
+++ interaction/output.go
@@ -3,6 +3,7 @@ package interaction
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/common-nighthawk/go-figure"
 )
@@ -57,7 +58,16 @@ func DeclareWinner(winner string) {
 }
 
 func WriteLogFile(rounds *[]RoundData) {
-	file, err := os.Create("gamelog.txt")
+	exPath, err := os.Executable()
+
+	if err != nil {
+		fmt.Println("Writing log file failed. Exiting!")
+		return
+	}
+
+	exPath = filepath.Dir(exPath)
+
+	file, err := os.Create(exPath + "gamelog.txt")
 
 	if err != nil {
 		fmt.Println("Saving a log file failed. Exiting.")
